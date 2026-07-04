f7784aab099e5101b71290a26a03001b0bee8198
Klaas <klaasotte99@gmail.com>
2026-07-04 17:36:19 +0200

    Server-checked single-use codes

--- docs/rettungshunde/flutter_bootstrap.js
+++ docs/rettungshunde/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2554938126" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3249049964" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
